905eec68508700edc764064dbf1faf77711cc250
Ashwini6kadam <kadamashwini717@gmail.com>
2024-09-30 13:51:31 +0530

    Add files via upload

---
+++ AgeCalculator.java
@@ -0,0 +1,34 @@
+package BuiltIn;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class AgeCalculator {
+	public static void main(String[] args) 
+	{
+		// Birthdate
+        LocalDate birthdate = LocalDate.of(2005, 07, 22);
+
+        // Current Date
+        LocalDate currentDate = LocalDate.now();
+
+        // Calculate Age
+        int age = calculateAge(birthdate, currentDate);
+
+        // Print Age
+        System.out.println("Age: " + age + " years");
+    }
+
+    public static int calculateAge(LocalDate birthdate, LocalDate currentDate) 
+    {
+        
+        Period period = Period.between(birthdate, currentDate);
+        
+          return period.getYears();
+    }
+	}
+
+
+
+
+
