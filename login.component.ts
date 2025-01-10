82ca4d50113fae1cb8fd9ac1e2dbffb28c5dcc8f
Ashwini6kadam <kadamashwini717@gmail.com>
2025-01-10 18:46:07 +0530

    Add files via upload

---
+++ login.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule,RouterModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  constructor(private router:Router){}
+
+  method2():void{
+    alert("Loging Sucessfully...!")
+    this.router.navigateByUrl('/home')
+
+  }
+  
+}
